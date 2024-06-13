de6748e90844b89a5e80634b38c437624693ed3a
Akeem Amusat <aoamusat@gmail.com>
2024-06-13 16:00:49 +0100

    Add jest unit testing

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,3 +2,5 @@ export const generateFixtureLink = (fixtureId: string): string => {
   const host = process.env.HOST || "http://localhost";
   return `${host}/fixtures/${fixtureId}`;
 };
+
+export const sum = (a: number, b: number): number => a + b;
